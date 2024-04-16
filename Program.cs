257e9d7158ddf79d1adaa7e9302592e0a2f7586a
Eric Nascimento <oiericoi@hotmail.com>
2024-04-16 15:22:17 -0300

    finalizando o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new("988221122", "Nokia tijolão", "AEI12345", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new("988221122", "iphone 4", "AEI9876", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
